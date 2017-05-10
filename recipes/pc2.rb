adf2b00d8a1d80574bc669f6c1d7c2d39b92de11
Rushinth John <rushinth@appranix.com>
2017-05-10 15:22:41 +0530

    Major Bug Fixes

--- recipes/pc2.rb
+++ recipes/pc2.rb
@@ -8,6 +8,7 @@ directory '/data/config/config-a' do
   owner 'root'
   group 'root'
   mode '0755'
+  recursive true
   action :create
 end
 
@@ -15,6 +16,7 @@ directory '/data/shard0/rs0' do
   owner 'root'
   group 'root'
   mode '0755'
+  recursive true
   action :create
 end
 
@@ -22,17 +24,18 @@ directory '/data/shard1/rs0' do
   owner 'root'
   group 'root'
   mode '0755'
+  recursive true
   action :create
 end
 
 execute 'Starting Config Server A' do
-  command '/home mongod --replSet cfgst --logpath "cfg-a.log" --dbpath /data/config/config-a --port 57040 --fork --configsvr'
+  command 'mongod --replSet cfgst --logpath "cfg-a.log" --dbpath /data/config/config-a --port 57040 --fork --configsvr'
 end
 
 execute 'Starting MongoD Shard-0 Replica-0' do
-  command '/home mongod --replSet s0 --logpath "s0-r0.log" --dbpath /data/shard0/rs0 --port 37017 --fork --shardsvr'
+  command 'mongod --replSet s0 --logpath "s0-r0.log" --dbpath /data/shard0/rs0 --port 37017 --fork --shardsvr'
 end
 
 execute 'Starting MongoD Shard-1 Replica-0' do
-  command '/home mongod --replSet s1 --logpath "s1-r0.log" --dbpath /data/shard1/rs0 --port 47017 --fork --shardsvr'
+  command 'mongod --replSet s1 --logpath "s1-r0.log" --dbpath /data/shard1/rs0 --port 47017 --fork --shardsvr'
 end
